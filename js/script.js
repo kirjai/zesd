e6adc749cf5214dbf5e2610339500f136cd049a2
Kirils Ladovs <kirjaitube@gmail.com>
2016-11-07 19:53:45 +0000

    feat: vkontakte social

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	var currentYear = new Date().getFullYear();
 	var releaseDate = new Date(2016, 10, 11);
-	var isReleased = new Date() >=releaseDate;
+	var isReleased = new Date() >= releaseDate;
 
 	jQuery('#year').html(currentYear);
 
@@ -10,6 +10,7 @@ $(document).ready(function(){
 	if (isReleased) {
 		releaseText = 'Out now';
 		jQuery('#released-on').show();
+		jQuery('.social .vkontakte').css('display', 'inline-block');
 	} else {
 		releaseText = 'Out 11th November';
 	}
